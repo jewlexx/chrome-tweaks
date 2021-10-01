7a9ae65968c548fe3c448e5d42655022481b4637
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-10-01 12:57:44 +1000

    Updates and fixes

--- youtube.js
+++ youtube.js
@@ -1,4 +1,10 @@
-document.onclick = () => {
-  const el = document.querySelector('[aria-label="Unsubscribe"]');
-  el?.click();
-};
+const unsubscribe = () =>
+  document.querySelector('[aria-label="Unsubscribe"]')?.click();
+
+const els = document.getElementsByClassName('ytd-subscribe-button-renderer');
+
+Array.from(els)
+  .map(el => el.parentElement())
+  .forEach(el => {
+    el.addEventListener('click', unsubscribe);
+  });
