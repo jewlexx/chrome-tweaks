4fa4ffa93da40367e76c90f153973676284bb2e3
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-10-03 16:20:43 +1100

    Updates

--- youtube.js
+++ youtube.js
@@ -4,3 +4,12 @@ const unsubscribe = async () =>
 Array.from(
   document.getElementsByClassName('ytd-subscribe-button-renderer'),
 ).forEach(async el => el.parentElement.addEventListener('click', unsubscribe));
+
+// Click on video title and it copies it
+
+if (window.location.pathname.includes('watch')) {
+  const el = document.querySelector('#container > h1 > yt-formatted-string');
+  el.addEventListener('click', e =>
+    navigator.clipboard.writeText(e.currentTarget.innerText),
+  );
+}
