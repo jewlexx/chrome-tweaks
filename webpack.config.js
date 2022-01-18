62c6c480ad3d4b85c09504ad24438f9bf7f4064b
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-18 18:33:54 +1100

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
 const path = require('path');
+const fs = require('fs');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
+module.exports = () => {
+  const dist = path.resolve(__dirname, 'dist');
+  fs.copyFileSync(
+    path.resolve(__dirname, 'manifest.json'),
+    path.resolve(dist, 'manifest.json'),
+  );
+
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      path: dist,
+      filename: 'bundle.js',
+    },
+  };
 };
