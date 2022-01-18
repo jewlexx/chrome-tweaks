277cbf2680206266902d16299510b1c3042adc48
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-18 18:11:44 +1100

    removed logs

--- src/markdown.ts
+++ src/markdown.ts
@@ -5,14 +5,10 @@ export async function markdown() {
   const isMd = mdReg.test(window.location.href);
 
   if (!isMd) {
-    console.log('Is not markdown... Goodbye :)');
     return;
   }
 
   const md = $('pre').text();
-
-  console.log(md);
-
   const html = marked(md);
 
   $('pre').html(html);
