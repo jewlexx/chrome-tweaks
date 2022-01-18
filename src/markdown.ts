00bcc8aa6a17835aea4f7e79c5404e56c0ed4d49
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-18 18:31:05 +1100

    fixes

--- src/markdown.ts
+++ src/markdown.ts
@@ -4,6 +4,12 @@ export async function markdown() {
   const md = $('pre').text();
   $('pre').remove();
 
+  const pre = $('<link rel="preconnect" href="https://fonts.gstatic.com">');
+  const rob = $(
+    '<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">',
+  );
+  $('head').append(pre, rob);
+
   const html = marked(md);
   const mdEl = $(`<div>${html}</div>`);
 
@@ -11,5 +17,6 @@ export async function markdown() {
     .append(mdEl)
     .css('display', 'flex')
     .css('flex-direction', 'column')
-    .css('align-items', 'center');
+    .css('align-items', 'center')
+    .css('font-family', 'Roboto');
 }
